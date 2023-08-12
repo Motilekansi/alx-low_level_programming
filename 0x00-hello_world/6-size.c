33bc166382d47f29a873a031f5c0853f4dfe8bcc
motilekansi <ismailsalaudeen05@gmail.com>
2023-08-12 06:05:51 -0700

    assigned values

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %c byte(s)", sizeof(char));
-	printf("size of an int: %c byte(s)", sizeof(int));
-	printf("size of a long int: %c byte(s)", sizeof(long int));
-	printf("size of a long long int: %c byte(s)", sizeof(long long int));
-	printf("size of a float: %c byte(s)", sizeof(float));
+	printf("size of a char: %c byte(s)\n", sizeof(char));
+	printf("size of an int: %c byte(s)\n", sizeof(int));
+	printf("size of a long int: %c byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %c byte(s)\n", sizeof(long long int));
+	printf("size of a float: %c byte(s)\n", sizeof(float));
 	return (0);
 }
